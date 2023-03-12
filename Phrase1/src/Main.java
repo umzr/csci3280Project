aeaa339268d641a039c2f6db6863df4fd1c41e48
umzr <lamshishing@gmail.com>
2023-03-12 18:22:47 +0800

    init: v2.0.0

--- Phrase1/src/Main.java
+++ Phrase1/src/Main.java
@@ -1,17 +1,5 @@
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
-
-
-        MainFrame run = new MainFrame();
-
-        run.setContentPane(run.MainGui);
-        run.setTitle("CSCI3280 Project Phrase 1 Music Player");
-        run.setSize(800, 600);
-        run.setVisible(true);
-        run.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
+        System.out.println("Hello world!");
     }
 }
---
+++ Phrase1/src/SongProperty.java
@@ -0,0 +1,12 @@
+public interface SongProperty {
+    public String getTitle();
+    public String getAuthor();
+    public String getDuration();
+    public String getChannels();
+    public String getRate();
+    public String getBits();
+    public String getAlbum();
+    public String getGenre();
+    public String getYear();
+    public String getComment();
+}
