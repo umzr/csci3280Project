4dbd1974e9c44798f4dc569675041d4e1d441c73
umzr <lamshishing@gmail.com>
2023-04-12 14:41:48 +0800

    comment not functioning coding

--- Phrase1/src/MusicDecoding.java
+++ Phrase1/src/MusicDecoding.java
@@ -1,9 +1,7 @@
 
-
-
 import java.io.File;
-        import java.io.FileInputStream;
-        import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Map;
 import javax.sound.sampled.*;
 
@@ -11,9 +9,8 @@ import javax.sound.sampled.*;
 
 public class MusicDecoding {
 
-
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException {
-         String test = "src/Album/01. senya — 華鳥風月.wav";
+        String test = "src/Album/01. senya — 華鳥風月.wav";
         File file = new File(test);
 
         try (FileInputStream fis = new FileInputStream(file)) {
@@ -53,7 +50,8 @@ public class MusicDecoding {
         System.out.println("Audio Sample Size: " + audioInputStream.getFormat().getSampleSizeInBits());
         System.out.println("Audio Big Endian: " + audioInputStream.getFormat().isBigEndian());
         System.out.println("Audio Length: " + audioInputStream.getFrameLength());
-        System.out.println("Audio Duration: " + audioInputStream.getFrameLength() / audioInputStream.getFormat().getFrameRate());
+        System.out.println(
+                "Audio Duration: " + audioInputStream.getFrameLength() / audioInputStream.getFormat().getFrameRate());
         System.out.println("Audio File Size: " + file.length());
         System.out.println("Title: " + fileFormat.properties().get("title"));
         System.out.println("Author: " + fileFormat.properties().get("author"));
@@ -63,7 +61,6 @@ public class MusicDecoding {
             System.out.println(entry.getKey() + " : " + entry.getValue());
         }
 
-
         String author = (String) properties.get("author");
         if (author != null) {
             System.out.println("Author: " + author);
@@ -71,36 +68,31 @@ public class MusicDecoding {
             System.out.println("Author not found in metadata");
         }
 
+        // TODO: 
+        // try {
 
+        // AudioFileFormat format = AudioSystem.getAudioFileFormat(file);
 
-
-        try {
-
-            AudioFileFormat format = AudioSystem.getAudioFileFormat(file);
-
-            // get the metadata from the format
-            if (format instanceof TAudioFileFormat) {
-                TAudioFileFormat tformat = (TAudioFileFormat)format;
-                Map<?, ?> properties = tformat.properties();
-                String title = (String)properties.get("title");
-                String artist = (String)properties.get("author");
-                String album = (String)properties.get("album");
-                String year = (String)properties.get("date");
-                // print the metadata
-                System.out.println("Title: " + title);
-                System.out.println("Artist: " + artist);
-                System.out.println("Album: " + album);
-                System.out.println("Year: " + year);
-            }
-        } catch (UnsupportedAudioFileException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // // get the metadata from the format
+        // if (format instanceof TAudioFileFormat) {
+        // TAudioFileFormat tformat = (TAudioFileFormat) format;
+        // Map<?, ?> properties = tformat.properties();
+        // String title = (String) properties.get("title");
+        // String artist = (String) properties.get("author");
+        // String album = (String) properties.get("album");
+        // String year = (String) properties.get("date");
+        // // print the metadata
+        // System.out.println("Title: " + title);
+        // System.out.println("Artist: " + artist);
+        // System.out.println("Album: " + album);
+        // System.out.println("Year: " + year);
+        // }
+        // } catch (UnsupportedAudioFileException e) {
+        // e.printStackTrace();
+        // } catch (IOException e) {
+        // e.printStackTrace();
+        // }
     }
-
-
-
 
     private static int byteArrayToInt(byte[] bytes, int offset) {
         return ((bytes[offset + 3] & 0xff) << 24) | ((bytes[offset + 2] & 0xff) << 16)
